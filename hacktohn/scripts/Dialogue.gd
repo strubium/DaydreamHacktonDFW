6571a94120f575ed114e6342056916311ef811bd
strubium <113206902+strubium@users.noreply.github.com>
2025-09-27 22:57:03 -0500

    Kill Everyone
    Co-Authored-By: Tony1547 <155920933+Tony1547@users.noreply.github.com>

--- hacktohn/scripts/Dialogue.gd
+++
@@ -1,9 +0,0 @@
-# Dialogue.gd
-class_name Dialogue
-
-var character : String
-var lines : Array
-
-func _init(character_name: String, dialogue_lines: Array):
-	character = character_name
-	lines = dialogue_lines

bd51d5413675d95fad178e4b8ccbbcb994399d89
strubium <113206902+strubium@users.noreply.github.com>
2025-09-29 15:38:10 -0500

    Skip build.bat, rename exe

--- build.bat
+++ build.bat
@@ -4,13 +4,14 @@ REM NW.js Windows Build Script
 REM -------------------------------
 
 REM Set variables
-set APP_NAME=MyGame
+set APP_NAME=SurfaceAndRepair
 set APP_VERSION=1.0.0
 set NW_JS_VERSION=0.82.0
 set BUILD_DIR=build
 set DIST_DIR=dist
 set NWJS_DIR=nwjs-sdk-v%NW_JS_VERSION%-win-x64
 set NWJS_ZIP=nwjs-sdk-v%NW_JS_VERSION%-win-x64.zip
+set BUILD_SCRIPT=build.bat
 
 REM Clean previous build
 echo Cleaning previous build...
@@ -19,9 +20,9 @@ rmdir /s /q %DIST_DIR%
 mkdir %BUILD_DIR%
 mkdir %DIST_DIR%
 
-REM Copy project files to build folder (exclude build/dist folders)
+REM Copy project files to build folder (exclude build/dist folders and the build script)
 echo Copying project files...
-robocopy . %BUILD_DIR% /E /XD %BUILD_DIR% %DIST_DIR%
+robocopy . %BUILD_DIR% /E /XD %BUILD_DIR% %DIST_DIR% /XF %BUILD_SCRIPT%
 
 REM Download NW.js if missing
 if exist %NWJS_DIR% (
@@ -38,9 +39,14 @@ REM Copy NW.js files to dist
 echo Copying NW.js runtime to dist...
 robocopy %NWJS_DIR% %DIST_DIR% /E
 
-REM Copy app files to dist
+REM Rename NW.js executable
+if exist "%DIST_DIR%\nw.exe" (
+    ren "%DIST_DIR%\nw.exe" "%APP_NAME%.exe"
+)
+
+REM Copy app files to dist (exclude the build script)
 echo Copying app files to dist...
-robocopy %BUILD_DIR% %DIST_DIR% /E
+robocopy %BUILD_DIR% %DIST_DIR% /E /XF %BUILD_SCRIPT%
 
 echo Build complete! Your app is in %DIST_DIR%\
 pause
